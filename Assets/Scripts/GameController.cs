19feb22b423c3c06d07e199342676481ab8c928d
JSchmidt1004 <67611141+JSchmidt1004@users.noreply.github.com>
2021-06-01 12:50:19 -0600

    GameController setup
    Basic things, don't know where to go from here

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public enum eState
+    {
+        Title,
+        LevelUp,
+        Fight
+    }
+
+    public eState State { get; set; } = eState.Title;
+
+    GameController instance;
+    public GameController Instance { get { return instance; } }
+
+    //public Robot[] = new Robot[2];
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        switch (State)
+        {
+            case eState.Title:
+                State = eState.LevelUp;
+                break;
+            case eState.LevelUp:
+                State = eState.Fight;
+                break;
+            case eState.Fight:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    //public void GetOtherRobot(Robot current)
+    //{
+
+    //}
+}
